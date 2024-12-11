32c96483ab0e48cb5aa224e7caa6ad0aae24f3a7
Geonwoo Lee <2dubu.dev@gmail.com>
2024-12-12 03:11:51 +0900

    🎉 :: tada

---
+++ RandomCats/RandomCats/RandomCatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomCatsApp.swift
+//  RandomCats
+//
+//  Created by 이건우 on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomCatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RandomCats/RandomCatsTests/RandomCatsTests.swift
@@ -0,0 +1,17 @@
+//
+//  RandomCatsTests.swift
+//  RandomCatsTests
+//
+//  Created by 이건우 on 12/12/24.
+//
+
+import Testing
+@testable import RandomCats
+
+struct RandomCatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
